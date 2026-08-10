f7c3d128cf5bad64db23e437920320964f42e763
SasidharanJana <sasidharan.janaranjan@gmail.com>
2026-08-10 12:40:31 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ki-billing-v4';
+const CACHE_NAME = 'ki-billing-v5';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
